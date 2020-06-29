b2bbf878b903b5bc7ad150731c27c96889f799ac
Lynxy <anton@timonenkov.ru>
2020-06-29 23:10:18 +0200

    Done

---
+++ Skillbox-13/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Skillbox-13
+//
+//  Created by Антон Тимоненков on 28.06.2020.
+//  Copyright © 2020 Антон Тимоненков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  var animationNumber = 0 {
+    willSet (newValue){
+      _animate(newValue)
+    }
+  }
+  var a = UIColor(), b = CGAffineTransform(), c = CGFloat(), d = CGFloat()
+  
+  @IBOutlet weak var squareView: UIView!
+  @IBOutlet weak var animationNumberLabel: UILabel!
+  @IBOutlet weak var rightConst: NSLayoutConstraint!
+  @IBOutlet weak var topConst: NSLayoutConstraint!
+  @IBOutlet weak var nextButton: UIButton!
+  @IBOutlet weak var prevButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
+  
+  @IBAction func prevAnimation(_ sender: Any) {
+    counter(false)
+  }
+  @IBAction func nextAnimation(_ sender: Any) {
+    counter(true)
+  }
+  @IBAction func stopAnimation(_ sender: Any) {
+    squareView.layer.removeAllAnimations()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    a = squareView.backgroundColor!
+    b = squareView.transform
+    c = squareView.layer.cornerRadius
+    d = squareView.alpha
+  }
+  
+  func counter(_ direction: Bool){
+    if direction {
+      if animationNumber + 1 == 8 { animationNumber = 1 } else { animationNumber += 1 }
+      animationNumberLabel.text = "\(animationNumber)"
+    }else{
+      if animationNumber - 1 == 0 { animationNumber = 7 } else { animationNumber -= 1 }
+      animationNumberLabel.text = "\(animationNumber)"
+    }
+  }
+  func _animate(_ animationNumber: Int){
+    self.prevButton.isHidden = true
+    self.nextButton.isHidden = true
+    self.stopButton.isHidden = false
+    var options = UIView.AnimationOptions()
+    if animationNumber == 6 {
+      self.squareView.transform = CGAffineTransform(scaleX: 2, y: 2)
+    }
+    if animationNumber == 7 {
+      options = [.repeat, .curveLinear]
+    }
+    UIView.animate(withDuration: 2, delay: 0, options: options, animations: {
+      switch animationNumber {
+      case 1: self.squareView.backgroundColor = .systemYellow
+      case 2: self.squareView.transform = CGAffineTransform(translationX: self.rightConst.constant, y: -self.topConst.constant)
+      case 3: self.squareView.layer.cornerRadius = self.squareView.frame.size.width/2
+      case 4: self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+      case 5: self.squareView.alpha = 0
+      case 6: self.squareView.transform = CGAffineTransform(scaleX: 1, y: 1)
+      case 7: self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+      default: return
+      }
+    }) { (isCompleted) in
+      self.squareView.backgroundColor = self.a
+      self.squareView.transform = self.b
+      self.squareView.layer.cornerRadius = self.c
+      self.squareView.alpha = self.d
+      self.prevButton.isHidden = false
+      self.nextButton.isHidden = false
+      self.stopButton.isHidden = true
+    }
+  }
+}
+
